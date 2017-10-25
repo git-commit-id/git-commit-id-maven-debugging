fad8e6921c0814e5997c8e68384b3b59ed9697e7
TheSnoozer <s.lange@alumni.tu-berlin.de>
2017-10-24 22:34:57 -0400

    move git commit-id-plugin into submodule for testing (issue 334)

---
+++ submodule-one/src/main/resources/sample.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# Project version: ${project.version}
+# Built by       : ${user.name}
+# Build time     : ${git.build.time}
+# Build on       : ${git.build.host} running ${os.name} (${os.version} and ${os.arch})
+# Built using    : ${java.version}
+#
+# Version info (GIT):
+# Commit ID   : ${git.commit.id}
+# Branch:     : ${git.branch}
+# Commit time : ${git.commit.time}
+# Committed by: ${git.commit.user.name}
+#
